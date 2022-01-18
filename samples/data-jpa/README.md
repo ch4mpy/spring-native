6ed5b34344f0074470ac50984bb1026efa8b48ed
ch4mpy <ch4mp@c4-soft.com>
2022-01-18 07:41:00 -1000

    Demo gh-963

--- samples/data-jpa/README.md
+++ samples/data-jpa/README.md
@@ -1,1 +1,7 @@
 Spring Boot project with Spring Data JPA.
+
+- `mvn clean install` => ok
+- `cd api && mvn spring-boot:run` => ok
+- `mvn spring-boot:build-image -Pnative-image` => KO
+- `mvn spring-boot:build-image -Pnative-image -DskipTests` => ok
+- `docker run --rm -p 8080:8080  -t data-jpa:0.0.1-SNAPSHOT` => KO
---
+++ samples/data-jpa/api/src/main/java/org/springframework/experimental/api/DataJpaApplication.java
@@ -0,0 +1,83 @@
+/*
+ * Copyright 2021 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.springframework.experimental.api;
+
+import static org.springframework.web.servlet.function.RequestPredicates.GET;
+import static org.springframework.web.servlet.function.RouterFunctions.route;
+import static org.springframework.web.servlet.function.ServerResponse.ok;
+
+import java.util.Optional;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.Bean;
+import org.springframework.data.domain.AuditorAware;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.experimental.domain.Flurb;
+import org.springframework.experimental.domain.Foo;
+import org.springframework.experimental.domain.FooRepository;
+import org.springframework.experimental.hierarchy.ComponentWithTransactionalInterface;
+import org.springframework.web.servlet.function.RouterFunction;
+
+@SpringBootApplication(scanBasePackageClasses = { ComponentWithTransactionalInterface.class, DataJpaApplication.class })
+@EnableJpaAuditing(auditorAwareRef = "fixedAuditor")
+@EntityScan(basePackageClasses = { Foo.class })
+@EnableJpaRepositories(basePackageClasses = FooRepository.class)
+public class DataJpaApplication {
+
+	private final FooRepository entities;
+
+	public DataJpaApplication(FooRepository entities) {
+		this.entities = entities;
+	}
+
+	@Bean
+	public CommandLineRunner runner() {
+		return args -> {
+
+			Optional<Foo> maybeFoo = entities.findById(1L);
+			Foo foo;
+			foo = maybeFoo.orElseGet(() -> entities.save(new Foo("Hello")));
+			Flurb flurb = new Flurb();
+			flurb.setValue("Balla balla");
+			foo.setFlurb(flurb);
+			entities.save(foo);
+
+			entities.findWithBetween("a", "X");
+		};
+	}
+
+	@Bean
+	AuditorAware<String> fixedAuditor() {
+		return () -> Optional.of("Douglas Adams");
+	}
+
+	@Bean
+	public RouterFunction<?> userEndpoints() {
+		return route(GET("/"), request -> ok().body(findOne()));
+	}
+
+	private Foo findOne() {
+		return entities.findById(1L).get();
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(DataJpaApplication.class, args);
+	}
+}
